491fac8aa73411f4b55123f42ccc8e397d085bf6
NugrahAnggaraS <bdaiunimed2024@gmil.com>
2025-06-02 20:23:47 +0700

    menambahkan allow origin untuk cors

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from controller.inquiry_controller import InquiryController
 from controller.auth_controller import AuthController
 from controller.data_ui_controller import DataUIController
+from fastapi.middleware.cors import CORSMiddleware
 import os
 
 load_dotenv()
@@ -13,6 +14,14 @@ auth_controller = AuthController()
 inquiry_controller = InquiryController()
 data_ui_controller = DataUIController()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth_controller.router)
 app.include_router(inquiry_controller.router)
 app.include_router(data_ui_controller.router)
